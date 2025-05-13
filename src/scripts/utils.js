bf960f3853e50c58f01ab0ec2558cdae46fc5b63
sid203 <siddhant.tandon203@gmail.com>
2025-05-13 12:49:41 +0200

    wrapping up - changed astro pages to collections

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,6 +1,8 @@
+import { getCollection } from "astro:content";
+
 export async function getUniqueTags() {
-    const allPosts = Object.values(import.meta.glob('../pages/posts/*.md', { eager: true }));
-    const listTags = allPosts.flatMap((post) => post.frontmatter.tags)
+    const allPosts = await getCollection("blog");
+    const listTags = allPosts.flatMap((post) => post.data.tags)
     const uniqueTags = [...new Set(listTags)];
   
     return uniqueTags
